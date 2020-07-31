e77231b3708040b6f45b724a91c165637e9891fa
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-07-31 08:51:42 +0700

    add field size

--- modules/geolocation/crud/domain/model/base.go
+++ modules/geolocation/crud/domain/model/base.go
@@ -8,11 +8,11 @@ import (
 
 // BaseModel represent BaseModel
 type BaseModel struct {
-	CreatedBy string         `json:"createdBy,omitempty" gorm:"column:sys_created_by" `
+	CreatedBy string         `json:"createdBy,omitempty" gorm:"column:sys_created_by;size:255" `
 	CreatedAt *time.Time     `json:"createdAt,omitempty" gorm:"column:sys_created_at" sql:"index"`
-	UpdatedBy string         `json:"updatedBy,omitempty" gorm:"column:sys_updated_by"`
+	UpdatedBy string         `json:"updatedBy,omitempty" gorm:"column:sys_updated_by;size:255"`
 	UpdatedAt *time.Time     `json:"updatedAt,omitempty" gorm:"column:sys_updated_at" sql:"index"`
-	DeletedBy string         `json:"deletedBy,omitempty" gorm:"column:sys_deleted_by"`
+	DeletedBy string         `json:"deletedBy,omitempty" gorm:"column:sys_deleted_by;size:255"`
 	DeletedAt gorm.DeletedAt `json:"deletedAt,omitempty" gorm:"column:deleted_at;index:idx_delete_at" sql:"index"`
 	// DeletedAt *time.Time `json:"deletedAt,omitempty" gorm:"column:sys_deleted_at" sql:"index"`
 }
