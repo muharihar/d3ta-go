e201041c2fdb906acebabb5e16ce647545b7701d
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-08-06 16:48:10 +0700

    move genSemVersion from module to GenSemVersion in system

--- system/utils/generator.go
+++ system/utils/generator.go
@@ -2,8 +2,10 @@ package utils
 
 import (
 	"encoding/base64"
+	"fmt"
 	"time"
 
+	"github.com/blang/semver/v4"
 	"github.com/google/uuid"
 )
 
@@ -36,4 +38,37 @@ func GenerateSecretKey() string {
 // GenerateRegistrationActivationCode generate RegistrationActivationCode
 func GenerateRegistrationActivationCode() string {
 	return GenerateUUID()
+}
+
+// GenSemVersion generate semversion
+func GenSemVersion(v string) string {
+	if v == "" {
+		v = "1.0.0"
+	} else {
+		ver, err := semver.Parse(v)
+		if err != nil {
+			ver, _ = semver.Parse("1.0.0")
+		}
+		// we use :
+		//     M.m(n).p(n)
+		//     1.10.10
+		//     x.<+.<+
+		if ver.Patch < 9 {
+			ver.Patch = ver.Patch + 1
+		} else {
+			if ver.Minor < 9 {
+				ver.Minor = ver.Minor + 1
+				ver.Patch = 0
+			} else {
+				if ver.Major < 9 {
+					ver.Major = ver.Major + 1
+					ver.Minor = 0
+					ver.Patch = 0
+				}
+			}
+		}
+		v = fmt.Sprintf("%v", ver)
+	}
+
+	return v
 }
