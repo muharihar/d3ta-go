09387bce41df889bc2b92e0f77ab7a2ec6a266d7
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-08-13 09:58:52 +0700

    TDD: implement assertion (system/initialize)

--- system/initialize/database_test.go
+++ system/initialize/database_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/muharihar/d3ta-go/system/config"
 	"github.com/muharihar/d3ta-go/system/handler"
+	"github.com/stretchr/testify/assert"
 )
 
 func newConfig(t *testing.T) (*config.Config, error) {
@@ -33,12 +34,35 @@ func newHandler(t *testing.T) (*handler.Handler, error) {
 }
 
 func TestLoadAllDatabaseConnection(t *testing.T) {
+
 	h, err := newHandler(t)
-	if err != nil {
-		t.Errorf("newHandler: %s", err.Error())
+	if assert.NoError(t, err, "Error while creating handler: newHandler") {
+		if !assert.NotNil(t, h) {
+			return
+		}
 	}
 
-	if err = LoadAllDatabaseConnection(h); err != nil {
-		t.Errorf("newHandler: %s", err.Error())
+	if assert.NoError(t, LoadAllDatabaseConnection(h), "Error while loading all database connection: LoadAllDatabaseConnection") {
+
+		cfg, err := h.GetConfig()
+		if !assert.NoError(t, err, "Error while getting config: h.GetConfig") {
+			return
+		}
+
+		dbCon, err := h.GetGormDB(cfg.Databases.IdentityDB.ConnectionName)
+		if assert.NoError(t, err, "Error while getting GORM DB Connection: h.GetGormDB(cfg.Databases.IdentityDB.ConnectionName)") {
+
+			type result struct {
+				DBName string //`json:"DBName" gorm:"column:db_name"`
+			}
+			results := []result{}
+
+			if assert.NoError(t, dbCon.Raw("SELECT DATABASE() db_name FROM DUAL").Scan(&results).Error) {
+				if assert.NotEmpty(t, results) {
+					assert.Equal(t, cfg.Databases.IdentityDB.DBName, results[0].DBName)
+				}
+				t.Logf("Result: %#v", results)
+			}
+		}
 	}
 }
