b72e4469f03fee68744e34b180435ee92da745f6
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-08-16 05:24:56 +0700

    TDD: implement assertion (system/config)

--- system/config/config_test.go
+++ system/config/config_test.go
@@ -1,22 +1,15 @@
 package config
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestConfig(t *testing.T) {
 	cfg, viper, err := NewConfig("../../conf")
-	if err != nil {
-		t.Errorf("LoadConfig.Error: %#v", err)
-	}
-
-	if viper == nil {
-		t.Errorf("Viper.Error: %#v", viper)
-	}
-
-	if cfg == nil {
-		t.Errorf("Config.Error: %#v", cfg)
-	}
-
-	if cfg != nil {
-		t.Logf("Config: %#v", cfg)
+	if assert.NoError(t, err, "Error while creating config: NewConfig") {
+		assert.NotNil(t, viper)
+		assert.NotNil(t, cfg)
 	}
 }
